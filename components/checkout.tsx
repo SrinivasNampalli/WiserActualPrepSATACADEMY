8630b296f2e96d344f560dab80259a3bf14a6259
SrinivasNampalli <Srinivarma9@gmail.com>
2026-01-23 08:06:33 -0800

    Fix Stripe publishable key env variable name

--- components/checkout.tsx
+++ components/checkout.tsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js"
 
 import { startCheckoutSession } from "@/app/actions/stripe"
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PK!)
 
 export function Checkout({ productId }: { productId: string }) {
   const startCheckoutSessionForProduct = useCallback(() => startCheckoutSession(productId), [productId])
